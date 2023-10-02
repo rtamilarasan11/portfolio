8f87b0ab5862e1769b1342fa9c166b4a7f5ec27e
Bewin007 <bewinfelix@karunya.edu.in>
2023-10-02 19:19:20 +0530

    a

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    extends: ['react-app'],
-    // Other ESLint configuration options can be added here
-
-    warnOnUnsupportedTypeScriptVersion: false,
-    reportUnusedDisableDirectives: true,
-  };
-  
